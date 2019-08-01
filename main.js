5f7714235b30c552c67f7317037e0e9fa64c26a5
giovanniASC6 <giovanni.owens@gmail.com>
2019-08-01 14:25:57 -0400

    visData

---
+++ main.js
@@ -0,0 +1,31 @@
+const {results} = data;
+
+//  console.log(results);
+
+ const [firstPerson,name,timezone,email,dob,phone] = results;
+
+//  let data = document.getElementById(`myData`)
+//  document.body.data = data.name
+document.getElementById
+ console.log(firstPerson);
+
+// fetch('data.json')
+// .then(function (response) {
+//     return response.json();
+// })
+// .then(function (data) {
+//     appendData(data);
+// })
+// .catch(function (err) {
+//     console.log('error: ' + err);
+// });
+
+// function appendData(data) {
+//     let mainContainer = document.getElementById("myData");
+//     for (let i = 0; i < data.length; i++) {
+//         let div = document.createElement("div");
+//         div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
+//         mainContainer.appendChild(div);
+//     }
+// }
+
